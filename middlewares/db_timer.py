7122a99fbe5cee64a29d62d2de4684d170f0aa4f
DragonsCode <84837099+DragonsCode@users.noreply.github.com>
2022-08-17 21:40:19 +0300

    Add files via upload

--- middlewares/db_timer.py
+++ middlewares/db_timer.py
@@ -1,1 +1,13 @@
+from vkbottle import BaseMiddleware
+from vkbottle.bot import Message
 
+class InfoMiddleware(BaseMiddleware[Message]):
+    async def post(self):
+        if not self.handlers:
+            self.stop("Сообщение не было обработано")
+
+        await self.event.answer(
+            "Сообщение было обработано:\n\n"
+            f"View - {self.view}\n\n"
+            f"Handlers - {self.handlers}"
+        )
